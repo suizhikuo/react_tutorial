0e6ab1cef1c6a002fedb8ffc7cb9ceb31f4dd257
suizk1 <suizk1@lenovo.com>
2021-05-17 23:11:19 +0800

    12313

--- 尚硅谷React项目教程(react实战全栈谷粒后台)/admin-client/src/App.js
+++ 尚硅谷React项目教程(react实战全栈谷粒后台)/admin-client/src/App.js
@@ -3,17 +3,27 @@
  */
 
 import React, {Component} from "react";
-
-import {Button, message} from 'antd';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './pages/login/login';
+import Admin from "./pages/admin/admin";
+// import {Button, message} from 'antd';
 
 export default class App extends React.Component {
 
-    handleClick = () => {
-        message.success('成功了!');
-    }
+    // handleClick = () => {
+    //     message.success('成功了!');
+    // }
 
     render() {
-        return <Button type="primary" onClick={this.handleClick}>测试antd</Button>
+        // return <Button type="primary" onClick={this.handleClick}>测试antd</Button>
+        return (
+            <BrowserRouter>
+                <Switch>{/* 只匹配其中一个 */}
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/' component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
 
+        )
     }
 }
---
+++ 尚硅谷React项目教程(react实战全栈谷粒后台)/admin-client/src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+/*
+登录的路由组件
+ */
+export default class Login extends Component {
+    render() {
+        return (
+            <div>login</div>
+        )
+    }
+}
