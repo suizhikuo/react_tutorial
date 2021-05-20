baf978432b597ed6c06491cbff9028746a1e1bb0
suizk1 <suizk1@lenovo.com>
2021-05-20 23:36:04 +0800

    kcuf

--- 尚硅谷React项目教程(react实战全栈谷粒后台)/admin-client/src/pages/login/login.jsx
+++ 尚硅谷React项目教程(react实战全栈谷粒后台)/admin-client/src/pages/login/login.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import './login.less';
+import logo from './images/bg.jpg';
 
 /*
 登录的路由组件
@@ -8,8 +9,13 @@ export default class Login extends Component {
     render() {
         return (
             <div className="login">
-                <header className="login-header"></header>
-                <section className="login-content"></section>
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React项目:后台管理系统</h1>
+                </header>
+                <section className="login-content">
+
+                </section>
             </div>
         )
     }
